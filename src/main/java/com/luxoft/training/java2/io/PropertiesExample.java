3bd70360f1fdc40fbd6b6ac18589cd5897a90278
Vyacheslav Lapin <Vyacheslav.Lapin@gmail.com>
2018-11-27 16:21:26 +0300

    Properties

---
+++ src/main/java/com/luxoft/training/java2/io/PropertiesExample.java
@@ -0,0 +1,19 @@
+package com.luxoft.training.java2.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public final class PropertiesExample {
+
+  public static void main(String... __) throws IOException {
+
+    var properties = new Properties();
+    try (InputStream resourceAsStream = PropertiesExample.class
+        .getResourceAsStream("/props.properties")) {
+      properties.load(resourceAsStream);
+    }
+
+    System.out.println(properties.getProperty("color")); // red
+  }
+}
---
+++ src/test/java/com/luxoft/training/java2/io/PropertiesExampleTest.java
@@ -0,0 +1,19 @@
+package com.luxoft.training.java2.io;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+class PropertiesExampleTest {
+  @Test
+  @DisplayName("\"Main\" method works correctly")
+  void testMain() throws IOException {
+    // given
+
+    // when
+    PropertiesExample.main(); // green
+    // then
+  }
+
+}
