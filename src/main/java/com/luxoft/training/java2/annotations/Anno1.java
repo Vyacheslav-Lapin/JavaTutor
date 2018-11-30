0a65f5b9312b121a56345d7958e8c4a02ecc752a
Vyacheslav Lapin <Vyacheslav.Lapin@gmail.com>
2018-12-01 01:26:43 +0300

    Annotations example

--- src/main/java/com/luxoft/training/java2/annotations/Anno1.java
+++ src/main/java/com/luxoft/training/java2/annotations/Anno1.java
@@ -7,7 +7,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Retention(RUNTIME)
 public @interface Anno1 {
   int x() default 5;
-  int y() default 15;
+  int[] ints() default 15;
 
   /**
    * @aliase for {@link #predicate()}
--- src/main/java/com/luxoft/training/java2/annotations/AnnotationExample.java
+++ src/main/java/com/luxoft/training/java2/annotations/AnnotationExample.java
@@ -2,10 +2,14 @@ package com.luxoft.training.java2.annotations;
 
 import lombok.experimental.FieldDefaults;
 
+import java.util.Arrays;
+
 import static lombok.AccessLevel.PRIVATE;
 
 @FieldDefaults(level = PRIVATE)
-@Anno1("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+@Anno1(
+    ints = 42,
+    predicate = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
 public final class AnnotationExample {
 
   public static void main(String... __) {
@@ -19,6 +23,6 @@ public final class AnnotationExample {
 
     System.out.println(predicate);
     System.out.println(annotation.x());
-    System.out.println(annotation.y());
+    System.out.println(Arrays.toString(annotation.ints()));
   }
 }
